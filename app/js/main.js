f6760ee49c0a5d30abf04ee743ac89ed4edd745e
Dmytro Anikin <dimianni@users.noreply.github.com>
2021-04-13 12:12:35 +0200

    Upgrade

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,24 @@
 
 document.addEventListener("DOMContentLoaded", function () {
 
+
+    function scrollTo(id) {
+
+        let element = document.getElementById(id);
+
+        let headerHeight = document.getElementById("header").offsetHeight;
+
+        let elementOffset = element.offsetTop;
+
+        let toScroll = elementOffset - headerHeight;
+
+        window.scrollTo({
+            top: toScroll,
+            behavior: "smooth"
+        })
+
+    }
+
     let links = document.querySelectorAll("a[href^='#']")
 
     links.forEach(link => {
@@ -9,9 +27,13 @@ document.addEventListener("DOMContentLoaded", function () {
             // Prevents default behavior (code below will not execute without this)
             e.preventDefault();
 
-            document.querySelector(this.hash).scrollIntoView({
-                behavior: "smooth"
-            })
+            let id = this.hash.replace("#", "")
+
+            // document.querySelector(this.hash).scrollIntoView({
+            //     behavior: "smooth"
+            // })
+
+            scrollTo(id);
 
         })
     })
